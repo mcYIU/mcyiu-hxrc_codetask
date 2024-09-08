1c1c73d6a186ebd6e95151048c86979999bfb7b5
mcYiu <112962373+mcYIU@users.noreply.github.com>
2024-09-08 16:19:53 +0800

    Edit "PlayerController"
    Add conditions of moving upward and destroying

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,14 +2,62 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float yThreshold;
+    public float force;
+    public ParticleSystem deadEffect;
+
+    private Rigidbody2D rb;
+    private bool canMove = true;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (canMove)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                // Handle player input
+                // Add force to the ball in the Y axis
+                rb.AddForce(Vector2.up * force, ForceMode2D.Impulse);
+            }
+
+            // Check if ball goes above the Y threshold
+            if (transform.position.y > yThreshold)
+            {
+                // Camera follows the ball up
+                Camera.main.transform.position = new Vector3(Camera.main.transform.position.x, transform.position.y, Camera.main.transform.position.z);
+            }
+
+            // Check if ball falls below the camera boundary
+            if (transform.position.y < Camera.main.ViewportToWorldPoint(new Vector3(0, 0, Camera.main.nearClipPlane)).y)
+            {
+                DestroyPlayer();
+            }
+        }
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("ColorSwitcher"))
+        string _tag = other.tag;
+
+        switch (_tag)
         {
-            // Change player color to a new color
-            ChangePlayerColor(other.GetComponent<ColorSwitcher>().SwitchColors);
-            // Destroy color switcher
-            Destroy(other.gameObject);
+            case "ColorSwitcher":
+                // Change player color to a new color
+                ChangePlayerColor(other.GetComponent<ColorSwitcher>().SwitchColors);
+                // Destroy color switcher
+                Destroy(other.gameObject);
+                break;
+
+            case "Obstacle":
+                other.TryGetComponent<SpriteRenderer>(out SpriteRenderer _renderer);
+                if (_renderer.color != GetComponent<SpriteRenderer>().color)
+                    DestroyPlayer();
+                break;
         }
     }
 
@@ -25,5 +73,13 @@ public class PlayerController : MonoBehaviour
         else
             // Get a new player color again
             ChangePlayerColor(_newColors);
+    }
+
+    private void DestroyPlayer()
+    {
+        Instantiate(deadEffect, transform.position, Quaternion.identity);        
+        Destroy(gameObject);
+
+        canMove = false;
     }
 }
