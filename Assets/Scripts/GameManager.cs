6690f5749e1137c2d4218d8479ce47818f79ec8a
mcYiu <112962373+mcYIU@users.noreply.github.com>
2024-09-09 01:43:22 +0800

    Update GameManager.cs
    Add the state of reload the scene when player tap either win or lose pages;
    Add UI components: buttons with end game statements;
    Enable the win page when the ball hits finish zone

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,35 +1,62 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public PlayerController player;
+    [Header("Colors Selection")]
     public Color[] availableColors;
-    public TextMeshProUGUI text_NumCollectedStars;
 
-    private static int _numCollectedStars = 0;
+    [Header("UI")]
+    public TextMeshProUGUI text_NumCollectedStars;
+    public GameObject winPage;
+    public GameObject losePage;
+
+    private static int numCollectedStars = 0;
     public static int NumCollectedStars
     {
-        get { return _numCollectedStars; }
+        get { return numCollectedStars; }
         set
         {
-            if (_numCollectedStars != value)
+            if (numCollectedStars != value)
             {
-                _numCollectedStars = value;
+                numCollectedStars = value;
                 // Load every time the static num changes
                 LoadNumCollectedStars();
             }
         }
     }
 
+    private void Start()
+    {
+        // Reset variables as Start
+        NumCollectedStars = 0;
+        winPage.SetActive(false);
+        losePage.SetActive(false);
+
+        // Enable the player ball's movement
+        PlayerController.canMove = true;
+    }
+
     private static void LoadNumCollectedStars()
     {
         // Update the TextMeshPro component with the new value of NumCollectedStars
-        FindObjectOfType<GameManager>().text_NumCollectedStars.text = _numCollectedStars.ToString();
+        FindObjectOfType<GameManager>().text_NumCollectedStars.text = numCollectedStars.ToString();
     }
 
-    public void DisplayWinStatement()
+    public void RestartGame()
     {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
 
+    public void OnEndPageEnable(string _hitTag)
+    {
+        // if the ball hits the finish zone
+        if (_hitTag == "Finish")
+            // player wins
+            winPage.SetActive(true);
+        else 
+            // player loses
+            losePage.SetActive(true);
     }
 }
